70b7ed3cd7f70501302c403894d5b8618ca352d0
John Mathena <bobtart41@gmail.com>
2025-03-07 20:29:58 -0500

    get data loader working

--- src/data/all-median-salaries.csv.py
+++ src/data/all-median-salaries.csv.py
@@ -1,16 +1,28 @@
+import os
 import polars as pl
 import sys
 
-from pathlib import Path
+EXCLUDED_AGENCY_IDS = [
+        "A35",
+        "N/A",
+        "SCS"
+        ]
 
-def main():
-    raw_salaries = pl.read_csv(Path(__file__).parent.parent.parent / "external_data/salaries-fy24.csv")
-    grouped_median_salaries = raw_salaries.group_by(
-            (pl.col("AgencyName"))
-    ).agg(
-        pl.col("AnnualSalary").median().round(2)
-    )
-    grouped_median_salaries.write_csv(sys.stdout)
-
-if __name__ == "__main__":
-    main()
+EXCLUDED_AGENCY_NAMES = [
+        "O01_ELJ",
+        "Environmental Control Board",
+        "Municipal Zoning & Appeals",
+        "N/A",
+        "Mayor's Office of Employment Development",
+        "Recreation & Parks - Administration",
+        "Recreation & Parks - Recreation"
+        "SCS - Special City Services",
+        "Transportation - Crossing Guards"
+        ]
+raw_salaries = pl.read_csv("src/data/salaries-fy24.csv")
+grouped_median_salaries = raw_salaries.filter(
+                ~pl.col("AgencyID").is_in(EXCLUDED_AGENCY_IDS),
+                pl.col("AnnualSalary") >= 200).group_by("AgencyName").agg(
+                        pl.col("AnnualSalary").median().round(2)
+                ).sort("AnnualSalary")
+grouped_median_salaries.write_csv(sys.stdout)
